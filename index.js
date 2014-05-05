d0cf6dddd7a14674a813dbe9108b13df9a7c0790
Michal Budzynski <michal@virtualdesign.pl>
2014-05-05 01:01:00 +0100

    [WIKI-INFOBOX] - :cow2: started writing own parser

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var request = require('request');
+//var XRegExp = require('xregexp').XRegExp;
 
 var page = "Warsaw";
 var apiURL = "http://en.wikipedia.org/w/api.php?format=json&action=query&prop=revisions&rvprop=content&titles=" + page;
@@ -15,7 +16,12 @@ request.get(apiURL, function(error, data, body){
   var page = Object.keys(content);
   content = content[page].revisions[0]['*'];
 
-  var regex = /\{\{\s*[Ii]nfobox +.*.*\}\}/s;
+  //var regex = XRegExp("\{\{\s*[Ii]nfobox +.*.*\}\}", "s");
+  //var data = XRegExp.exec(content, regex);
 
-  console.log(content.match(regex));
+  var startingPointRegex = /\{\{\s*[Ii]nfobox/;
+
+  var macz = content.match(startingPointRegex).index;
+
+  console.log(content.substr(macz, content.length);
 });

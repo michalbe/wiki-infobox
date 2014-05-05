ecd8428d7adc7bc7000c4f271741f2837db43b41
Michal Budzynski <michal@virtualdesign.pl>
2014-05-05 01:01:00 +0100

    [WIKI-INFOBOX] - :fu: Api call to Wikipedia

---
+++ index.js
@@ -0,0 +1,13 @@
+var request = require('request');
+
+var page = "Warsaw";
+var apiURL = "http://en.wikipedia.org/w/api.php?format=json&action=query&prop=revisions&rvprop=content&titles=" + page;
+
+request.get(apiURL, function(error, data, body){
+  if (error) {
+    console.log('ERROR: ', error);
+    return;
+  }
+
+  console.log(apiURL);
+});

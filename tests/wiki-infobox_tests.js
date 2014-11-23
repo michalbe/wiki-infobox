7853d3d9d21571cbc73bdb26ffe55501404ac968
Michał Budzyński <michal@virtualdesign.pl>
2014-11-23 01:01:00 +0100

    [WIKI-INFOBOX] - :diamonds: non-wiki pages tests

--- tests/wiki-infobox_tests.js
+++ tests/wiki-infobox_tests.js
@@ -7,6 +7,7 @@ var assert = require('assert');
 
 var language = 'en';
 var page = 'Bemowo';
+var oldPage;
 
 var initMock = function(body) {
   nock('http://'+ language + '.wikipedia.org')
@@ -186,8 +187,16 @@ wikiInfobox(page, language, function(err, data) {
 });
 
 //Test special chars in the title
+oldPage = page;
 page='Franklin & Bash';
 initMock(require('./mocks/10.js'));
 wikiInfobox(page, language, function(err, data) {
   assert.deepEqual(data, {name: {type:'text',value:'Franklin & Bash'}});
+});
+page = oldPage;
+
+initMock(require('./mocks/11.js'));
+wikiInfobox(page, language, function(err, data) {
+  assert.ok(err);
+  assert.equal(data, undefined);
 });
